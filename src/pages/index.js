6ddab89b3d93c1641ac753f3421b2cc06eca15ad
Mariia Riabinina <m.riabinina@mobidev.biz>
2022-02-17 19:09:02 +0200

    removed a photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,6 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-      <StaticImage
         alt="morning-tea"
         src="https://i.ibb.co/7tx3rDZ/kelsey-curtis-1570052-unsplash.jpg"
       />
